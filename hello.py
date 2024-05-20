f13b6ed566d27b8a2c33d7f1b2cbac9db4fd28f3
Jacques <jacques.gerome@gmail.com>
2024-05-20 20:12:50 +0200

    hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,24 @@
-print(3+10)
+print("Hello, World!")
 
+firstname = input("What is your name? ")
+
+print(f"Welcome {firstname}")
+
+a = int(input("please write an number"))
+b = int(input("please write an number"))
+
+print("sum equals: ")
+print( a + b)
+
+print("difference equals: ")
+print( a - b)
+
+print("product equals: ")
+print( a * b)
+
+print("quotient equals: ")
+print( a / b)
+
+age = int(input("What is your age?"))
+
+print(f"Next year, you will be {age+1} years old")

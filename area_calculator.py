cf17afab635099e7805553db83d45f4d9a5466b9
Jacques <jacques.gerome@gmail.com>
2024-05-20 19:36:10 +0200

    first commit

---
+++ area_calculator.py
@@ -0,0 +1,6 @@
+x = int(input("what is side one of the rectangle?"))
+y = int(input("what is side two of the rectangle?"))
+
+print("the area of the rectangle is: ")
+print( x * y)
+
---
+++ groceries.py
@@ -0,0 +1,13 @@
+eggs = 2
+banana = 2
+steak_and_rice = 30
+salad = 10
+
+print("breakfast cost")
+print(eggs + banana)
+
+print("lunch cost")
+print(salad)
+
+print("dinner cost")
+print(steak_and_rice)

44a6134ca4a13a1336e72409141c5e5ed4007abb
Jacques <jacques.gerome@gmail.com>
2024-05-21 16:50:45 +0200

    shapese

---
+++ shapes.py
@@ -0,0 +1,31 @@
+x = int(input("length of rectangle: "))
+y = int(input("width of rectangle: "))
+
+print("enter the length of the rectangle: ")
+print(int(x))
+print("enter the width of the rectangle: ")
+print(int(y))
+print("")
+print("area of rectangle: ")
+print( x * y)
+print("")
+
+import math
+
+r = int(input("radius of circle: "))
+print("enter radius of circle: ")
+print(int(r))
+print(int( 2 * math.pi * r))
+print("")
+
+l = int(input("length of triangle: "))
+h = int(input("height of triangle: "))
+
+print("enter the base of the triangle: ")
+print(int(l))
+print("enter the height of the triangle: ")
+print(int(h))
+print("")
+print("area of triangle: ")
+print((l * h)/2)
+print("")
